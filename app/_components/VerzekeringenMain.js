a0733fa4fff5e71253fb2df62753fe0ce8c8c42d
Quincymahn <quincy@mahn.it>
2025-09-30 14:32:23 +0000

    verzekeringen pagina image update

--- app/_components/VerzekeringenMain.js
+++ app/_components/VerzekeringenMain.js
@@ -28,7 +28,7 @@ function VerzekeringenMain() {
           <div className="absolute inset-0 bg-gray-300 rounded-2xl blur-md"></div>
           <div className="relative flex flex-col items-center w-full h-full px-6 py-8 bg-white rounded-2xl md:-rotate-2">
             <Image
-              src="/img/Generated Image September 11, 2025 - 10_11AM.png"
+              src="/img/Generated-Image-September-11-2025-10_11AM.png"
               width={170}
               height={170}
               alt="Car icon"
